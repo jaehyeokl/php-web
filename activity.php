19b3b24e185d296b49810ff9547e29e4fc0f212d
root <root@ip-172-31-39-226.ap-northeast-2.compute.internal>
2020-11-10 15:32:18 +0000

    추천활동 페이지 웹진게시판 형태 및 css 구현

--- activity.php
+++ activity.php
@@ -12,7 +12,7 @@
     <meta property="og:title" content="ego lego" />
     <meta property="og:description" content="활동적인 아웃도어 라이프스타일" />
     <title>추천 활동</title>
-    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/activity.css">
     <script src="https://kit.fontawesome.com/8451689280.js" crossorigin="anonymous"></script>
 </head>
 
@@ -43,7 +43,70 @@
             ?>
         </ul>
     </nav>
-    
+
+    <!-- 추천 활동 -->
+    <section class="section__body">
+        <header class="section__header">
+            섹션 헤더
+        </header>
+
+        <section class="section__list">
+            <ul>
+                <li>
+                    <a href="#" class="thumbnail">
+                        <img src="/images/cedebb6e872f539bef8c3f919874e9d7.jpg" alt>
+                    </a>
+                    <div class="contents">
+                        <h4 class="contents__title">
+                            <a href="#">타이틀을 작성</a>
+                        </h4>
+                        <p class="contents__preview">
+                            <a href="#">컨텐츠 내용 미리보기, 미리보기내용입니다ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd</a>
+                        </p>
+                        <span class="contents__info">
+                            <em>카테고리 | </em>
+                            <em>작성일</em>
+                        </span>
+                    </div>
+                    <div class="thumb">
+                        <i class="fas fa-heart"></i>
+                        <span>99</span>
+                    </div>
+                </li>
+
+
+                <li>
+                    <a href="#" class="thumbnail">
+                        <img src="/images/cedebb6e872f539bef8c3f919874e9d7.jpg" alt>
+                    </a>
+                    <div class="contents">
+                        <h4 class="contents__title">
+                            <a href="#">타이틀을 작성</a>
+                        </h4>
+                        <p class="contents__preview">
+                            <a href="#">컨텐츠 내용 미리보기, 미리보기내용입니다ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd</a>
+                        </p>
+                        <span class="contents__info">
+                            <em>카테고리 | </em>
+                            <em>작성일</em>
+                        </span>
+                    </div>
+                    <div class="thumb">
+                        <i class="fas fa-heart"></i>
+                        <span>99</span>
+                    </div>
+                </li>
+            </ul>
+        </section>
+        <div class="pages">
+            <ul>
+                <li>1</li>
+            </ul>
+        </div>
+
+        
+
+    </section>
 
 </body>
 </html>
