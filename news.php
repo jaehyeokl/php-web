559ad5bb42ac9e49cc97036b8005b12e3e6c93ce
root <root@ip-172-31-39-226.ap-northeast-2.compute.internal>
2020-11-17 01:55:50 +0900

    관련 뉴스 페이지 뉴스목록 구현

--- news.php
+++ news.php
@@ -2,16 +2,27 @@
   include 'login_session.php';
 ?>
 
+<?php
+    // news 데이터 불러오기
+    $json_string_news = file_get_contents("tourlist.json");
+    // JSON문자열을 배열로 변환
+    $array_news = json_decode($json_string_news, true);
+    // var_dump($array_news);
+    foreach($array_news as $news) {
+        $news_tag = $news_tag."<li><a href='".$news[link]."'>"."<span class='title'>".$news[title]."</span>"."<span class='content'>".$news[content]."</span></a></li>";
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="ko">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="메인페이지">
+    <meta name="description" content="관련뉴스">
     <meta property="og:title" content="ego lego" />
     <meta property="og:description" content="활동적인 아웃도어 라이프스타일" />
-    <title>메인</title>
+    <title>관련뉴스</title>
     <link rel="stylesheet" href="css/news.css">
     <script src="https://kit.fontawesome.com/8451689280.js" crossorigin="anonymous"></script>
 </head>
@@ -43,10 +54,14 @@
             ?>
         </ul>
     </nav>
-    <!-- <video muted autoplay>
-        <source src="video/index.mp4" type="video/mp4">
-        <strong>Your browser does not support the video tag.</strong>
-    </video> -->
-</body>
 
+    <!-- 뉴스 목록 -->
+    <div class="news">
+        <h2>관련 뉴스</h2>
+        <ul>
+            <?= $news_tag ?>
+        </ul>
+    </div>
+    
+</body>
 </html>
